ecd501a9dea7121bb7e067242580d5ac44365ae8
Cintia Coccia <cintia_08@hotmail.com>
2023-08-19 19:28:02 +0200

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './zucca-theme.css'
 import './App.css'
-import NavBar from './Components/NavBar/NavBar';
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <NavBar/>
       <Routes>
         <Route path='/login'></Route>
         <Route path='/home'></Route>
