29e9edb17da7e48befe35e8e0e0a529bbdcfde6a
Cintia Coccia <cintia_08@hotmail.com>
2023-08-20 13:10:15 +0200

    Home component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './zucca-theme.css'
 import './App.css'
+import Home from './Components/Home/Home';
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <div className='App'>
       <Routes>
         <Route path='/login'></Route>
-        <Route path='/home'></Route>
+        <Route path='/home' element={<Home />}></Route>
         <Route path='/appointment'></Route>
         <Route path='/signup'></Route>
       </Routes>
