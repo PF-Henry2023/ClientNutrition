ddd4624851f64418f8d242cb20c2bfce8a9752fe
spadonienzo <spadonienzo@gmail.com>
2023-08-21 15:08:54 -0300

    Calendario renderizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './zucca-theme.css'
-import './App.css'
+import { useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./zucca-theme.css";
+import "./App.css";
+import Calendar from "./Components/Calendar/Calendar";
 
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/login'></Route>
-        <Route path='/home'></Route>
-        <Route path='/appointment'></Route>
-        <Route path='/signup'></Route>
+        <Route path="/login"></Route>
+        <Route path="/home"></Route>
+        <Route path="/appointment" element={<Calendar />}></Route>
+        <Route path="/signup"></Route>
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import style from "./Calendar.module.css";
+
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    start: moment().toDate(),
+    end: moment().add(0.1, "days").toDate(),
+    title: "An event",
+  },
+];
+
+const Calendar = () => {
+  return (
+    <div className={style.Calendar}>
+      {/* <Navbar/> */}
+      <div>
+        <button className={style.Agendar}>Agendar mi cita</button>
+      </div>
+      <div>
+        <BigCalendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={events}
+          style={{ height: "80vh" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
