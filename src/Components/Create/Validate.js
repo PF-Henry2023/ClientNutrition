0d1152bf89ea1bc9e48590f5019728dd1ba7fac1
MReyesSegura <7588818@gmail.com>
2023-08-22 15:53:40 -0600

    Mejoras en el form

--- src/Components/Create/Validate.js
+++ src/Components/Create/Validate.js
@@ -2,28 +2,31 @@ const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{6,}$/;
 const numberRegex = /^[1-9]\d{9}$/;
 const wordRegex = /^[A-Za-z\s]+$/;
+const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
 
-export default function Validate(input) {
+export function Validate(input) {
     const errors = {};
-    if (input.name === "") errors.name = true;
-    else if (!wordRegex.test(input.name)) errors.name1 = true;
+    
+  if (!wordRegex.test(input.name) && input.name.length > 0) errors.name1 = true;
 
-    if (input.lastName === "") errors.lastName = true;
-    else if (!wordRegex.test(input.lastName)) errors.lastName1 = true;
+  if (!wordRegex.test(input.lastName) && input.lastName.length > 0)
+    errors.lastName1 = true;
 
-    if (input.phone === "") errors.phone1 = true;
-    else if (!numberRegex.test(input.phone)) errors.phone = true;
+  if (!numberRegex.test(input.phone) && input.phone.length > 0)
+    errors.phone1 = true;
 
-    if (!emailRegex.test(input.email)) errors.email = true;
+  if (!emailRegex.test(input.email) && input.email.length > 0)
+    errors.email1 = true;
 
-    if (!passwordRegex.test(input.password)) errors.password = true;
+  if (!dateRegex.test(input.birthDate) && input.birthDate.length > 0)
+    errors.birthDate1 = true;
 
-    if (input.gender === "") errors.gender = true;
+  if (!passwordRegex.test(input.password) && input.password.length > 0)
+    errors.password1 = true;
 
-    if (input.address === "") errors.address = true;
+  return errors;
+}
 
-    if (input.birthDate === "") errors.birthDate = true;
 
 
-    return errors;
-} 
+
