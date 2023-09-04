8d71a6146f6d9b1d4a07dd65afe40d82dcfc5e0f
Cintia Coccia <cintia_08@hotmail.com>
2023-09-04 11:13:54 +0200

    styling

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,50 +1,40 @@
-import styles from "./Cards.module.css"
+import styles from "./Cards.module.css";
 import Card from 'react-bootstrap/Card';
 
 export default function Cards() {
     return (
         <div className={styles.container}>
-        <div className={styles.text}>
-            <div className={styles.title}>QUIENES NOS ELIGEN</div>
-
-            <div className="row">
-                <div className="col-md-4">
-                    <Card className="h-100" style={{ width: '18rem', backgroundColor: 'lightgreen' }}>
-                        <Card.Body>
-                            <Card.Title>Carlos M.</Card.Title>
-                            {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
-                            <Card.Text>
-                                "Gracias a ZUCCA finalmente tengo un plan alimenticio hecho a mi medida. Me siento más energizado y apoyado en mi camino hacia el bienestar. Ha sido una experiencia transformadora!"
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-
-                <div className="col-md-4">
-                    <Card className="h-100" style={{ width: '18rem', backgroundColor: 'green' }}>
-                        <Card.Body>
-                            <Card.Title>Diego T.</Card.Title>
-                            {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
-                            <Card.Text>
-                                "Como deportista, ZUCCA ha mejorado mi rendimiento con planes personalizados. Mi energía y recuperación están en su punto máximo. Es altamente recomendable, yo estoy muy conforme."
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-
-                <div className="col-md-4">
-                    <Card className="h-100" style={{ width: '18rem', backgroundColor: "#FF0000" }}>
-                        <Card.Body>
-                            <Card.Title>Dra. Lucia G.</Card.Title>
-                            {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
-                            <Card.Text>
-                                "ZUCCA ha revolucionado mi enfoque como profesional. Facilita la comunicación y me permite concentrarme en guiar a mis pacientes hacia sus objetivos."
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
+            <div className={styles.text}>
+                <div className={styles.title}>QUIENES NOS ELIGEN</div>
             </div>
-        </div>
+            <div className={styles.cardContainer}>
+                <Card className="h-100" style={{ width: '18rem', backgroundColor: 'lightgreen' }}>
+                    <Card.Body>
+                        <Card.Title>Carlos M.</Card.Title>
+                        <Card.Text>
+                        "Gracias a ZUCCA finalmente tengo un plan alimenticio hecho a mi medida. Me siento más energizado y apoyado en mi camino hacia el bienestar. Ha sido una experiencia transformadora. Desde que comencé a seguir el plan de nutrición personalizado de ZUCCA, he logrado perder peso de manera sostenible y mejorar mi salud en general. La atención personalizada y el seguimiento constante de los profesionales de ZUCCA me han ayudado a mantenerme motivado y enfocado en mis objetivos de salud. Estoy muy agradecido por la experiencia y los resultados que he obtenido con ZUCCA. ¡Lo recomiendo altamente a cualquiera que busque un cambio positivo en su vida!"                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card className="h-100" style={{ width: '18rem', backgroundColor: 'green' }}>
+                    <Card.Body>
+                        <Card.Title>Diego T.</Card.Title>
+                        <Card.Text>
+                        "Como deportista, ZUCCA ha tenido un impacto transformador en mi rendimiento y bienestar. Gracias a los planes de nutrición personalizados diseñados por los expertos de ZUCCA, mi energía y recuperación están en su punto máximo. Antes de unirme a ZUCCA, estaba luchando por mantener un equilibrio adecuado entre mi entrenamiento riguroso y mi dieta. Constantemente me sentía agotado y mis tiempos de recuperación eran lentos.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card className="h-100" style={{ width: '18rem', backgroundColor: "#fd7e14" }}>
+                    <Card.Body>
+                        <Card.Title>Dra. Lucia G.</Card.Title>
+                        <Card.Text>
+                            "ZUCCA ha revolucionado mi enfoque como profesional. Facilita la comunicación y me permite concentrarme en guiar a mis pacientes hacia sus objetivos. En el tiempo que llevo en la plataforma estoy sorprendida por la facilidad que plantea este sistema integral para llevar a mis pacientes a sus metas. Claramente es una nueva forma de encarar la nutrición de la que me enorgullece ser parte"
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     )
 }
+
