1b6e041f5b2cd5be1e2450b90568a7067d02c850
Henry Corredor González <henry.corredor7570@gmail.com>
2023-09-05 17:53:42 -0500

    actualizado cloudinary

--- src/Components/Cloudinary/Cloudinary.jsx
+++ src/Components/Cloudinary/Cloudinary.jsx
@@ -3,24 +3,28 @@ import { Container, FormGroup, Input } from "reactstrap";
 import styles from "./Cloudinary.module.css";
 import cloudinaryConfig from './CloudinaryCredentials';
 
-
-const Cloudinary = () => {
-
+const Cloudinary = ({name, lastName}) => {
+    
     const [file, setFile] = useState("");//estado que se usa para almacenar la url del archivo subido a cloudinar
     const [loading, setloading] = useState(false);// se utiliza para controlar si se está cargando un archivo o no
     const [uploadSuccess, setUploadSuccess] = useState(false);//estado para indicar la carga exitosa
+    console.log(file);
     
     const handleUploadFile = async (event) => {// se ejecuta cuando se selecciona un archivo en el campo de entrada.
         event.preventDefault();
-        const selectedFile = event.target.files[0];//se obtiene la lista de archivos seleccionados por el usuario
+        let selectedFile = event.target.files[0];//se obtiene la lista de archivos seleccionados por el usuario
         const data = new FormData();//Se crea un objeto FormData para construir los datos que se enviarán a Cloudinary.
-        // const publicId = "minombrepersonalizado.jpg"; // Cambia por el nombre deseado
-        
+        console.log(selectedFile);
+        let fileNew = await selectedFile
+        console.log(fileNew);
+        fileNew.name = `${name} ${lastName}.${fileNew.name.split(".")[1]}`
+
         data.append("file", selectedFile);
         data.append("upload_preset", "filesZucca");//filesZucca es el nombre de la carpeta que se creo en cloudinary
         data.append("public_id", selectedFile.name);//para especificar el nombre del archivo en Cloudinary
         setloading(true);//Cuando se está cargando un archivo, loading se establece en true,
-
+        
+        
         try {
             const response = await fetch(
                 `https://api.cloudinary.com/v1_1/${cloudinaryConfig.cloudName}/upload`,
--- src/Components/Cloudinary/Download.jsx
+++ src/Components/Cloudinary/Download.jsx
@@ -4,13 +4,13 @@ import axios from 'axios';
 
 const DownloadCloudinary = () => {
   const [secureDownloadUrl, setSecureDownloadUrl] = useState('');
-  console.log(secureDownloadUrl);
+  const [downloadSuccess, setDownloadSuccess] = useState(false); // Estado para controlar el mensaje de descarga exitosa
+
   useEffect(() => {
     // Realiza una solicitud al servidor para obtener la URL segura
     axios.get('http://localhost:3001/obtener-url-segura')
       .then((response) => {
-        console.log(response.data);
-        setSecureDownloadUrl(response.data);//.secureDownloadUrl
+        setSecureDownloadUrl(response.data);//
       })
       .catch((error) => {
         console.error('Error al obtener la URL segura:', error);
@@ -21,11 +21,16 @@ const DownloadCloudinary = () => {
     // Cuando los usuarios hagan clic en el botón de descarga,
     // redirige a la URL segura para la descarga.
     window.location.href = secureDownloadUrl;
+
+    // Cuando se inicie la descarga, establece el estado de descarga exitosa en verdadero.
+    setDownloadSuccess(true);
   };
 
   return (
     <div>
       <button onClick={handleDownloadClick}>Descargar Notas de Consulta</button>
+      {/* Muestra el mensaje de descarga exitosa si downloadSuccess es verdadero */}
+      {downloadSuccess && <p>Descarga exitosa. ¡Gracias por descargar el archivo!</p>}
     </div>
   );
 }
